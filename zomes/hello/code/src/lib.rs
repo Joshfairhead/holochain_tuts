f0b9767879db065a58ff72735d977dbe36b0cadc
Josh <JoshAFairhead@gmail.com>
2020-03-01 21:55:20 +0100

    Completed Holochain Hello Holo Tutorial

---
+++ zomes/hello/code/src/lib.rs
@@ -0,0 +1,37 @@
+#![feature(proc_macro_hygiene)]
+extern crate hdk;
+extern crate hdk_proc_macros;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate holochain_json_derive;
+
+use hdk::{
+    error::ZomeApiResult,
+};
+
+use hdk_proc_macros::zome;
+
+// see https://developer.holochain.org/api/0.0.42-alpha5/hdk/ for info on using the hdk library
+
+// This is a sample zome that defines an entry type "MyEntry" that can be committed to the
+// agent's chain via the exposed function create_my_entry
+
+#[zome]
+mod hello_zome {
+
+    #[init]
+    fn init() {
+        Ok(())
+    }
+
+    #[validate_agent]
+    pub fn validate_agent(validation_data: EntryValidationData<AgentId>) {
+        Ok(())
+    }
+    #[zome_fn("hc_public")]
+    pub fn hello_holo() -> ZomeApiResult<String> {
+        Ok("Hello Holo".into())
+    }
+}
